ef6fca93b56389c0a51ae034ce793eb293751034
Khishignuur1 <56242931+Khishignuur1@users.noreply.github.com>
2020-04-12 19:54:53 +0100

    Add files via upload

---
+++ base.py
@@ -0,0 +1,14 @@
+"""
+Written by 17075800
+tests was not being recognized as package so I have decided to put this here.
+"""
+from flask_testing import TestCase
+
+from run import app
+
+#Setting the config to Testconfig for unit tests
+class BaseTestCase(TestCase):
+
+    def create_app(self):
+        app.config.from_object('config.TestConfig')
+        return app
