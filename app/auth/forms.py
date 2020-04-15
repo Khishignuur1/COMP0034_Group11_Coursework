b908c5db6064be3ec9befba403620c6bc644ec67
Khishignuur1 <56242931+Khishignuur1@users.noreply.github.com>
2020-04-15 09:01:15 +0100

    Update forms.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -10,6 +10,7 @@ from flask_login import current_user
 from app.models import User
 
 
+# Creates a Signup form
 class SignupForm(FlaskForm):
     name = StringField('Username', validators=[DataRequired()])
     email = StringField('Email address', validators=[DataRequired(), Email(message='Valid email address required')])
@@ -17,41 +18,47 @@ class SignupForm(FlaskForm):
                              validators=[DataRequired(), EqualTo('confirm', message='Passwords must match')])
     confirm = PasswordField('Repeat Password')
 
+    # Validates the username's uniqueness (queries the database for entered username and presents error if match found)
     def validate_name(self, name):
         user = User.query.filter_by(name=name.data).first()
         if user:
             raise ValidationError('This username is taken. Please choose a different one.')
-
+            
+    # Validates the email's uniqueness (queries the database for entered email and presents error if match found)
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('This email is taken. Please choose a different one.')
 
-
+            
+# Creates a Login Form
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
 
 
+# Creates an account update form
 class UpdateAccountForm(FlaskForm):
     name = StringField('Username', validators=[DataRequired()])
     email = StringField('Email address', validators=[DataRequired(), Email(message='Valid email address required')])
     submit = SubmitField('Update')
-
+    
+    # Validates the username's uniqueness (queries the database for entered username and presents error if match found)
     def validate_name(self, name):
         if name.data != current_user.name:
             user = User.query.filter_by(name=name.data).first()
             if user:
                 raise ValidationError('This username is taken. Please choose a different one.')
-
+    
+    # Validates the email's uniqueness (queries the database for entered email and presents error if match found)
     def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('This email is taken. Please choose a different one.')
 
-
+# Creates an Item form
 class ItemForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
